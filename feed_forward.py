Fixed softmax bugs and added in backprop and cost functions

feed_forward.py
@@ -1,5 +1,5 @@
 import numpy as np
-from math import sqrt
+from math import sqrt,log
 
 class FeedForwardNet():
     '''FeedForwardNet
@@ -8,7 +8,7 @@ class FeedForwardNet():
     and nonlinearities
     '''
 
-    def __init__(self, layers, transforms, cost='maxent'):
+    def __init__(self, layers, transforms, cost='xent'):
         '''___init___
         initialze the feed forward neural network
 
@@ -29,13 +29,13 @@ class FeedForwardNet():
             for the model (ie. 1000)
 
         cost -> string - objective function. Default to
-            'maxent' for Maximum Entropy; can use 'mse'
+            'xent' for Cross Entropy; can use 'mse'
             for mean squared error
         '''
         assert len(layers) > 1, "Must have at least an input and output layer"
         assert len(layers) == len(transforms)+1, "Must have one less transform than layers (because you don't include the input layer in transforms)"
 
-        self.max_entropy = True if cost == 'maxent' else False
+        self.x_entropy = True if cost == 'xent' else False
 
         # inputs to the first layer
         # AKA dimensionality of feature
@@ -51,10 +51,12 @@ class FeedForwardNet():
         self.activations = list(map(lambda x: np.zeros(x), layers[1:]))
         self.deltas = list(map(lambda x: np.zeros(x), layers[1:]))
 
+        self.biases = []
         self.weights = []
         for i in range(1, len(layers)):
             dist = 1 / sqrt(layers[i-1])
-            self.weights.append(np.random.rand(layers[i-1]+1, layers[i])*2*dist - dist) # draw from Unif(-dist, dist)
+            self.weights.append(np.random.rand(layers[i-1], layers[i])*2*dist - dist) # draw from Unif(-dist, dist)
+            self.biases.append(np.random.rand(1, layers[i])*2*dist - dist)
 
     def _forward(self, x):
         '''_forward
@@ -64,15 +66,187 @@ class FeedForwardNet():
 
         >>> import nonlinearity as nl
         >>> nn = FeedForwardNet(layers=[3,2,3,1], transforms=[nl.ReLu, nl.ReLu, nl.Sigmoid])
-        >>> nn.weights = [np.array([[0,0],[-3,3],[2,5],[10,2]]), np.array([[-10,3,-5],[-2,3,12],[6,1,2]]), np.array([[-20],[0.001],[1],[0.25]])]
+        >>> nn.weights = [np.array([[-3,3],[2,5],[10,2]]), np.array([[-2,3,12],[6,1,2]]), np.array([[0.001],[1],[0.25]])]
+        >>> nn.biases = [np.array([0,0]),np.array([-10,3,-5]),np.array([-20])]
         >>> nn._forward(np.array([[1,2,-1],[-1,3,2]])).T
         array([[ 0.15525053,  1.        ]])
         '''
         # add a column of ones for the bias term
-        self.activations[0] = self.transforms[0].f(np.column_stack((np.ones(x.shape[0]), x)).dot(self.weights[0]))
+        self.activations[0] = self.transforms[0].f(x.dot(self.weights[0]) + self.biases[0])
 
         for i in range(1, len(self.layers)-1):
             # calculate activation
-            self.activations[i] = self.transforms[i].f(np.column_stack((np.ones(self.activations[i-1].shape[0]), self.activations[i-1])).dot(self.weights[i]))
+            self.activations[i] = self.transforms[i].f(self.activations[i-1].dot(self.weights[i]) + self.biases[i])
 
         return self.activations[-1]
+
+    def _backward(self, x, y):
+        '''_backward
+        Run a backward pass through the network,
+        returning the a list of numpy matrices
+        of the derivative of the cost function
+        with respect to each layer's weights.
+
+        The function assumes that the forward
+        pass result is stored in self.activations[-1]
+
+        >>> import nonlinearity as nl
+        >>> nn = FeedForwardNet(layers=[3,2,3,1], transforms=[nl.ReLu, nl.ReLu, nl.Sigmoid], cost='mse')
+        >>> nn.weights = [np.array([[-3,3],[2,5],[10,2]]), np.array([[-2,3,12],[6,1,2]]), np.array([[0.001],[1],[0.25]])]
+        >>> nn.biases = [np.array([0,0]),np.array([-10,3,-5]),np.array([-20])]
+        >>> x = np.array([[1,2,-1]])
+        >>> y_hat = nn._forward(x) # compute forward pass
+        >>> grad_w, grad_b = nn._backward(x, 1)
+        >>> print(grad_w)
+	[array([[ 0.        , -0.16684527],
+	       [ 0.        , -0.33369055],
+	       [ 0.        ,  0.16684527]]), array([[ 0.        ,  0.        ,  0.        ],
+	       [-0.00121866, -1.21865738, -0.30466435]]), array([[-6.20407395],
+	       [-1.55101849],
+	       [-1.88337959]])]
+        >>> print(grad_b)
+	[array([[-0.        , -0.16684527]]), array([[-0.00011079, -0.11078703, -0.02769676]]), array([[-0.11078703]])]
+        '''
+        # compute deltas (starting with last layer)
+        if self.x_entropy: # assume using sigmoid or softmax
+            self.deltas[-1] = (self.activations[-1] - y)
+        else:
+            self.deltas[-1] = (self.activations[-1] - y) * self.transforms[-1].df(self.activations[-1])
+
+        # iterate through the layers backwards
+        for i in range(2, len(self.layers)):
+            self.deltas[-i] = np.dot(self.deltas[-i+1], self.weights[-i+1].T) * self.transforms[-i].df(self.activations[-i])
+
+        # calculate gradients
+        grad_w = [x.T.dot(self.deltas[0])]
+        grad_b = [self.deltas[0].copy()]
+        for i in range(1, len(self.deltas)):
+            grad_w.append(self.activations[i-1].T.dot(self.deltas[i]))
+            grad_b.append(self.deltas[i].copy())
+        return grad_w, grad_b
+
+    def cost(self, x, y):
+        '''cost
+        Cost function computation on the neural
+        network. If the cost of the network is
+        'x_ent' then use cross entropy. Otherwise
+        use mean squared error.
+
+        x is an input array or vector of features
+        where each row is another example and each
+        column is a feature input.
+
+        y is the label of the result as a matrix/vector
+        with just as many rows as x.
+
+        Mean Squared Error Tests:
+        >>> import nonlinearity as nl
+        >>> nn = FeedForwardNet(layers=[3,2,3,1], transforms=[nl.ReLu, nl.ReLu, nl.Sigmoid], cost='mse')
+        >>> nn.weights = [np.array([[-3,3],[2,5],[10,2]]), np.array([[-2,3,12],[6,1,2]]), np.array([[0.001],[1],[0.25]])]
+        >>> nn.biases = [np.array([0,0]),np.array([-10,3,-5]),np.array([-20])]
+        >>> x = np.array([[1,2,-1],[-1,3,2]])
+        >>> nn.cost(x, np.array([[1],[1]]))
+        0.17840041878634177
+        >>> nn.cost(np.array([1,2,-1]), 1)
+        0.35680083757268355
+        >>> nn.cost(np.array([-1,3,2]), 1)
+        0.0
+
+        Cross Entropy Cost Tests:
+        >>> import nonlinearity as nl
+        >>> nn = FeedForwardNet(layers=[2,2,3], transforms=[nl.ReLu, nl.Softmax])
+        >>> nn.weights = [np.array([[1,3],[2,-1]]), np.array([[1,3,5],[2,4,6]])]
+        >>> nn.biases = [np.array([3,-6]),np.array([2,-10,-8])]
+        >>> x = np.array([[1,-1],[100,-1]])
+        >>> nn._forward(np.array([[1,-1],[100,-1]]))
+	array([[  8.80536902e-01,   2.95387223e-04,   1.19167711e-01],
+               [  0.00000000e+00,   0.00000000e+00,   1.00000000e+00]])
+        >>> nn.cost(x, np.array([[0,0,1],[0,0,1]]))
+        2.127223441901406
+        >>> nn.cost(np.array([1,-1]), np.array([0,0,1]))
+        2.127223441901406
+        >>> nn.cost(np.array([100,-1]), np.array([0,0,1]))
+	-0.0
+
+        >>> import nonlinearity as nl
+        >>> nn = FeedForwardNet(layers=[3,2,3,1], transforms=[nl.ReLu, nl.ReLu, nl.Sigmoid])
+        >>> nn.weights = [np.array([[-3,3],[2,5],[10,2]]), np.array([[-2,3,12],[6,1,2]]), np.array([[0.001],[1],[0.25]])]
+        >>> nn.biases = [np.array([0,0]),np.array([-10,3,-5]),np.array([-20])]
+        >>> x = np.array([[1,2,-1],[-1,3,2]])
+        >>> nn.cost(x, np.array([[1],[1]]))
+	1.8627151751310125
+        >>> nn.cost(np.array([1,2,-1]), 1)
+	1.8627151751310125
+        >>> nn.cost(np.array([-1,3,2]), 1)
+	-0.0
+        '''
+        # do some casting to make computation
+        # efficient (give np axis of {0,1})
+        if not hasattr(y, '__len__'):
+            y = np.array(y)
+            y.shape = (1,1)
+        if not hasattr(x, '__len__'):
+            x = np.array(x)
+            x.shape = (1,1)
+        if len(x.shape) == 1:
+            x.shape = (1, x.shape[0])
+        if len(y.shape) == 1:
+            y.shape = (1, y.shape[0])
+            
+        y_hat = self._forward(x)
+        if self.x_entropy:
+            if self.layers[-1] == 1: # bernoilli output
+                zeros = np.invert(y == 1)
+                y_hat[zeros] = 1 - y_hat[zeros]
+                return -1*np.log(y_hat).sum()
+            # else multinomial output
+            return -1*np.log(y_hat[np.arange(len(y_hat)), y.argmax(axis=1)]).sum()
+        else: #use mean squared error
+            return np.sum(np.square(np.linalg.norm(y - y_hat, axis=1))) / (2*len(x))
+        
+
+    def _numerical_grad(self, x, y, epsilon):
+        '''_numerical_grad
+        Computes grad_w, grad_b numerically. This
+        is to be used for gradient checking primarily.
+        The method uses the central difference:
+
+            f'(x) = (f(x+epsilon) - f(x-epsilon)) / (2*epsilon)
+
+        >>> import nonlinearity as nl
+        >>> nn = FeedForwardNet(layers=[3,2,3,1], transforms=[nl.ReLu, nl.ReLu, nl.Sigmoid], cost='mse')
+        >>> nn.weights = [np.array([[-3,3],[2,5],[10,2]]), np.array([[-2,3,12],[6,1,2]]), np.array([[0.001],[1],[0.25]])]
+        >>> nn.biases = [np.array([0,0]),np.array([-10,3,-5]),np.array([-20])]
+        >>> x = np.array([[1,2,-1]])
+        >>> y_hat = nn._forward(x) # compute forward pass
+        >>> grad_w, grad_b = nn._numerical_grad(x, 1, 1e-4)
+        >>> print(grad_w)
+	[array([[ 0.        , -0.16684527],
+	       [ 0.        , -0.33369055],
+	       [ 0.        ,  0.16684527]]), array([[ 0.        ,  0.        ,  0.        ],
+	       [-0.00121866, -1.21865738, -0.30466435]]), array([[-6.20407395],
+	       [-1.55101849],
+	       [-1.88337959]])]
+        >>> print(grad_b)
+	[array([[-0.        , -0.16684527]]), array([[-0.00011079, -0.11078703, -0.02769676]]), array([[-0.11078703]])]
+        '''
+        grad_w = [np.zeros_like(w) for w in self.weights]
+        grad_b = [np.zeros_like(b) for b in self.biases]
+        for l in range(len(self.weights)):
+            for (i,j) in np.ndindex(self.weights[l].shape):
+                self.weights[l][i,j] += epsilon
+                right = self.cost(x,y)
+                self.weights[l][i,j] -= 2*epsilon
+                left = self.cost(x,y)
+                grad_w[l][i,j] = (right - left) / (2*epsilon)
+                self.weights[l][i,j] += epsilon
+                
+        for l in range(len(self.biases)):
+            for i in np.ndindex(self.biases[l].shape):
+                self.biases[l][i] += epsilon
+                right = self.cost(x,y)
+                self.biases[l][i] -= 2*epsilon
+                left = self.cost(x,y)
+                grad_b[l][i] = (right - left) / (2*epsilon)
+                self.biases[l][i] += epsilon
+        return grad_w, grad_b

nonlinearity.py
@@ -1,3 +1,14 @@
+'''nonlinearity
+file creates classes for common neural network
+nonlinearities. All classes have 2 static methods:
+
+    f(x numpy.ndarray) -> numpy.ndarray
+        evaluate the nonlinearity on a numpy array
+
+    df(y numpy.ndarray) -> numpy.ndarray
+        evaluate the nonlinearity on a numpy array
+        _FROM THE OUTPUT_ of f(x)
+'''
 import numpy as np
 from scipy.misc import logsumexp
 from math import tanh, exp
@@ -31,10 +42,10 @@ class Tanh():
         d/dx tanh(x) = 1 - tanh^2(x)
 
         >>> import numpy
-        >>> Tanh.df(np.array([-999, 0, 999]))
+        >>> Tanh.df(Tanh.f(np.array([-999, 0, 999])))
         array([ 0.,  1.,  0.])
         '''
-        return 1 - np.square(np.tanh(x))
+        return 1 - np.square(x)
 
 class Sigmoid():
     '''Sigmoid
@@ -63,11 +74,10 @@ class Sigmoid():
 
         >>> import numpy
         >>> _ = numpy.seterr(over='ignore', under='ignore')
-        >>> Sigmoid.df(np.array([-999, 0, 999]))
+        >>> Sigmoid.df(Sigmoid.f(np.array([-999, 0, 999])))
         array([ 0.  ,  0.25,  0.  ])
         '''
-        f = np.reciprocal(1 + np.exp(-1*x))
-        return f * (1 - f)
+        return x * (1 - x)
 
 class ReLu():
     '''ReLu
@@ -96,7 +106,7 @@ class ReLu():
         at x
 
         >>> import numpy
-        >>> ReLu.df(numpy.array([-10, -1, 1, 10])) + 0 # add 0 to case from bool to int
+        >>> ReLu.df(ReLu.f(numpy.array([-10, -1, 1, 10]))) + 0 # add 0 to case from bool to int
         array([0, 0, 1, 1])
         '''
         return x > 0
@@ -128,7 +138,7 @@ class Identity():
         evaluated at x
 
         >>> import numpy
-        >>> Identity.df(numpy.array([-1,0,1]))
+        >>> Identity.df(Identity.f(numpy.array([-1,0,1])))
         array([1, 1, 1])
         '''
         return np.ones_like(x)
@@ -152,19 +162,33 @@ class Softmax():
 
         >>> Softmax.f(np.array([-999, 1, 1]))
         array([ 0. ,  0.5,  0.5])
-        '''
-        maximum = np.maximum(0, x)
-        exp =  np.exp(x)
-        return np.exp(x - maximum - logsumexp(x - maximum, axis=1))
 
+        >>> Softmax.f(np.array([[-999,1,1],[1,-999,1],[1,1,-999],[1,1,1]]))
+	array([[ 0.        ,  0.5       ,  0.5       ],
+	       [ 0.5       ,  0.        ,  0.5       ],
+	       [ 0.5       ,  0.5       ,  0.        ],
+	       [ 0.33333333,  0.33333333,  0.33333333]])
+        '''
+        x = x.astype('float64')
+        shape = x.shape
+        if len(x.shape) == 1:
+            x.shape = (1,x.shape[0])
+        mx =  np.maximum(0.0, x.max(axis=1))
+        mx.shape = (1, mx.shape[0])
+        x -= mx.T
+        x -= logsumexp(x, axis=1, keepdims=True)
+        x.shape = shape
+        return np.exp(x)
+
+    @staticmethod
     def df(x):
         '''df
         Softmax derivative with respect to x
 
         (used maximum entropy so derivative is 1)
 
-        >>> Softmax.df(np.array([-100, 0, 1]))
-        array([1, 1, 1])
+        >>> Softmax.df(Softmax.f(np.array([-100, 0, 1])))
+        array([ 1.,  1.,  1.])
         '''
         return np.ones_like(x)
 
@@ -178,8 +202,7 @@ class LogSoftmax():
     the output.
 
     uses a numerically stable variant to prevent
-    float overflow (not underflow) from
-        http://timvieira.github.io/blog/post/2014/02/11/exp-normalize-trick/
+    float overflow (not underflow)
 
     f(x)_i = log( exp(x_i) / sum_j( exp(x) ) )
     '''
@@ -191,19 +214,32 @@ class LogSoftmax():
         with a numpy array x
 
         >>> LogSoftmax.f(np.array([-999, 1, 1]))
-        array([ -9.99693147e+02,  -6.93147181e-01,  -6.93147181e-01])
-        '''
-        maximum = np.maximum(0, x)
-        exp =  np.exp(x)
-        return x - maximum - logsumexp(x - maximum, axis=1)
+        array([ -1.00069315e+03,  -6.93147181e-01,  -6.93147181e-01])
 
+        >>> LogSoftmax.f(np.array([[-999,1,1],[1,-999,1],[1,1,-999]]))
+        array([[ -1.00069315e+03,  -6.93147181e-01,  -6.93147181e-01],
+               [ -6.93147181e-01,  -1.00069315e+03,  -6.93147181e-01],
+               [ -6.93147181e-01,  -6.93147181e-01,  -1.00069315e+03]])
+        '''
+        x = x.astype('float64')
+        shape = x.shape
+        if len(x.shape) == 1:
+            x.shape = (1,x.shape[0])
+        mx =  np.maximum(0.0, x.max(axis=1))
+        mx.shape = (1, mx.shape[0])
+        x -= mx
+        res = x - logsumexp(x, axis=1)
+        res.shape = shape
+        return res
+
+    @staticmethod
     def df(x):
         '''df
         LogSoftmax derivative with respect to x
 
         (used maximum entropy so derivative is 1)
 
-        >>> LogSoftmax.df(np.array([-100, 0, 1]))
-        array([1, 1, 1])
+        >>> LogSoftmax.df(LogSoftmax.f(np.array([-100, 0, 1])))
+        array([ 1.,  1.,  1.])
         '''
         return np.ones_like(x)